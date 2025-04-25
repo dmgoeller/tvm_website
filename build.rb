ebf1f328831596789b70bed649290d2699951f5b
dmgoeller <denis@dmgoeller.de>
2025-04-25 19:09:08 +0200

    Refactored build.rb

--- build.rb
+++ build.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+# frozen_string_literal: true
+
 require 'optparse'
 require 'cssminify'
 require 'net/ftp'
@@ -23,13 +25,13 @@ end.parse!
 
 # utility methods
 
-def action(name, &block)
-  puts "#{name}"
+def action(name)
+  puts name
   yield
 end
 
-def task(name, &block)
-  puts ['', "#{'-' * 40}", name, "#{'-' * 40}", '']
+def task(name)
+  puts ['', '-' * 40, name, '-' * 40, '']
   yield $config[name.downcase.gsub(' ', '_')] || {}
 end
 
@@ -46,21 +48,22 @@ def uri_escape(s)
 end
 
 def hex_to_rgba(hex)
-  decimals = [1, 3, 5, 7].map { |i| hex[i..(i+1)].to_i(16) }
+  decimals = [1, 3, 5, 7].map { |i| hex[i..(i + 1)].to_i(16) }
   decimals[3] = (decimals[3]).to_f / 256 # alpha
   "rgba(#{decimals.join(',')})"
 end
 
 def deep_merge(h1, h2)
-  h1.dup.tap { |merged|
+  h1.dup.tap do |merged|
     h2.each_pair do |key, value|
-      if (merged[key].is_a?(Hash) && value.is_a?(Hash))
-        merged[key] = deep_merge(merged[key], value)
-      else
-        merged[key] = value
-      end
+      merged[key] =
+        if merged[key].is_a?(Hash) && value.is_a?(Hash)
+          deep_merge(merged[key], value)
+        else
+          value
+        end
     end
-  }
+  end
 end
 
 def convert_images(src, dest, options)
@@ -86,13 +89,14 @@ def ftp_upload(ftp, local_dir, remote_dir, options = {})
     remote_filenames = ftp.nlst.map { |filename| filename }
 
     Dir.entries('.').each do |filename|
-      next if %w(. .. .DS_Store).include?(filename) || exclude&.match(filename)
+      next if %w[. .. .DS_Store].include?(filename) || exclude&.match(filename)
 
       if File.directory?(filename)
         ftp.mkdir(filename) unless remote_filenames.include?(filename)
         ftp_upload(ftp, filename, filename, options)
       else
         next if remote_filenames.include?(filename) && ftp.mtime(filename) >= File.mtime(filename)
+
         puts "Upload #{remote_dir}/#{filename}"
         ftp.putbinaryfile(filename)
       end
@@ -131,9 +135,9 @@ def copy(src, dest, options = {})
   action "Copy files from \'#{src}\' to \'#{dest}\'" do
     FileUtils.copy_entry(src, dest)
 
-    options[:exclude].each do |exclude|
+    options[:exclude]&.each do |exclude|
       FileUtils.rm_r("#{dest}/#{exclude}")
-    end unless options[:exclude].nil?
+    end
   end
 end
 
@@ -142,89 +146,91 @@ def build_html(filename, options = {})
     html = File.read(filename)
 
     # replace base path
-    html.gsub!(/data\-app\-base\-path\s*\=\s*\"(\w|\/)*\"/) { |chunk|
+    html.gsub!(/data-app-base-path=".*"/) do
       "data-app-base-path=\"#{options[:base_path]}\""
-    }
+    end
     # replace canonical path
-    html.gsub!(/data\-app\-canonical\-path\s*\=\s*\"(\w|\/)*\"/) { |chunk|
+    html.gsub!(/data-app-canonical-path=".*"/) do
       "data-app-canonical-path=\"#{options[:canonical_path]}\""
-    }
+    end
     # replace robots meta property
-    html.gsub!(/\<meta\s+name\=\"robots\"\s+content\=\"(\w|\s)*\"\>/) {
+    html.gsub!(/<meta\s+name="robots"\s+content="(\w|\s)*">/) do
       "<meta name=\"robots\" content=\"#{options[:meta_robots]}\">"
-    } unless options[:meta_robots].nil?
+    end unless options[:meta_robots].nil?
     # embed stylesheets
-    html.gsub!(/\<link.*href\=\"assets\/stylesheets\/(\w|\-)*\.css\".*\>/) { |link|
-      stylesheet = File.read(link.match(/(\w|\-|\/)*\.css/)[0])
+    html.gsub!(%r{<link.*href="assets/stylesheets/(\w|-)*\.css".*>}) do |link|
+      stylesheet = File.read(link.match(%r{(\w|-|/)*\.css})[0])
       stylesheet.gsub!(/#[0-9a-fA-F]{8}/) { |color| hex_to_rgba(color) }
       stylesheet.gsub!('url(../../fonts/', 'url(fonts/')
       stylesheet = CSSminify.compress(stylesheet)
       "<style>\n#{stylesheet}\n</style>"
-    }
+    end
     # embed javascripts
-    html.gsub!(/\<script.*src\=\"assets\/javascripts\/(\w|\-)*\.js\".*\>/) { |script|
-      javascript = File.read(script.match(/(\w|\-|\/)*\.js/)[0])
+    html.gsub!(%r{<script.*src="assets/javascripts/(\w|-)*\.js".*>}) do |script|
+      javascript = File.read(script.match(%r{(\w|-|/)*\.js})[0])
       javascript = Uglifier.compile(javascript, harmony: true)
       "<script type=\"text/javascript\">\n#{javascript}\n</script>"
-    }
+    end
     # embed placeholder images
-    html.gsub!(/data-image\=\"(\w|\-|\/)*\.jpg\"\s*data-placeholder-image/) { |chunk|
-      placeholder_image = "#{chunk.match(/(\w|\-|\/)*\.jpg/)[0]}".sub('media', '../thumbs')
+    html.gsub!(%r{data-image="(\w|-|/)*\.jpg"\s*data-placeholder-image}) do |chunk|
+      placeholder_image = chunk.match(%r{(\w|-|/)*\.jpg})[0].sub('media', '../thumbs')
       image_size = FastImage.size(placeholder_image)
       svg = String.new(options[:placeholder_image_template])
+      # rubocop:disable Lint/InterpolationCheck
       svg.sub!('#{height}', !image_size.nil? ? image_size[1].to_s : '27')
       svg.sub!('#{width}', !image_size.nil? ? image_size[0].to_s : '48')
       svg.sub!('#{base64}', [File.binread(placeholder_image)].pack('m'))
+      # rubocop:enable Lint/InterpolationCheck
       "#{chunk} style=\"background-image: url(data:image/svg+xml,#{uri_escape(svg)});\""
-    }
+    end
     File.write(filename, html)
-  end
-end
-
-def build_service_worker_js(article_dir, options = {})
-  action "Build 'service-worker.js'" do
-    service_worker_js = File.read('service-worker.js')
-
-    service_worker_js.gsub!(/ARTICLES_TO_CACHE\s\=\s\[.*\]/) {
-      articles = article_names(article_dir).map { |article| "'articles/#{article}.html'"}
-      "ARTICLES_TO_CACHE = [#{articles.join(',')}]"
-    }
-    File.write('service-worker.js', service_worker_js)
   end
 end
 
 def build_robots_txt(options = {})
   action "Build 'robots.txt'" do
-    robots_txt = "User-agent: *\nDisallow: /index.php\nDisallow: /images/\n\n"
-    robots_txt << "Sitemap: #{options[:canonical_path]}/sitemap.xml"
-    File.write('robots.txt', robots_txt)
+    File.write(
+      'robots.txt',
+      <<~ROBOTS_TXT
+        User-agent: *
+        Disallow: /index.php
+        Disallow: /images/
+
+        Sitemap: #{options[:canonical_path]}/sitemap.xml
+      ROBOTS_TXT
+    )
   end
 end
 
 def build_sitemap_xml(articles_dir, options = {})
   action "Build 'sitemap.xml'" do
     lastmod = Time.now.strftime('%Y-%m-%d')
-    File.write('sitemap.xml', Nokogiri::XML::Builder.new { |xml|
-      xml.urlset xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9' do
-        article_names(articles_dir).each do |article_name|
-          article_name = '' if article_name == options[:index_page]
-          xml.url do
-            xml.loc "#{options[:canonical_path]}/#{article_name}"
-            xml.lastmod lastmod
+    File.write(
+      'sitemap.xml',
+      Nokogiri::XML::Builder.new do |xml|
+        xml.urlset xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9' do
+          article_names(articles_dir).each do |article_name|
+            article_name = '' if article_name == options[:index_page]
+            xml.url do
+              xml.loc "#{options[:canonical_path]}/#{article_name}"
+              xml.lastmod lastmod
+            end
           end
         end
-      end
-    }.to_xml )
+      end.to_xml
+    )
   end
 end
 
 def build_htaccess(articles_dir, options = {})
   action "Build '.htaccess'" do
+    # rubocop:disable Style/FormatStringToken
     redirect_http_to_https = <<~REDIRECT_HTTP_TO_HTTPS
       # Redirect http to https
       RewriteCond %{HTTPS} !=on
       RewriteRule ^ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]
     REDIRECT_HTTP_TO_HTTPS
+    # rubocop:enable Style/FormatStringToken
 
     htaccess = <<~HTACCESS
       RewriteEngine On
@@ -258,18 +264,18 @@ def build_htaccess(articles_dir, options = {})
     end
 
     htaccess << "\n\# Error pages\n"
-    Dir.glob('*.html').grep(/\d{3}\.html/) { |filename|
+    Dir.glob('*.html').grep(/\d{3}\.html/) do |filename|
       htaccess << "ErrorDocument #{filename[0, 3]} /#{filename}\n"
-    }
+    end
     File.write('.htaccess', htaccess)
   end
 end
 
 def build_thumbnail_images(src, dest)
   action 'Build thumbnail images' do
-    convert_images(src, dest, "-thumbnail 48")
-    #FileUtils.copy_entry(src, dest)
-    #system("find #{dest} -type f -name '*.jpg' -exec sips -Z 48 {} \\;", out: :close, err: :close)
+    convert_images(src, dest, '-thumbnail 48')
+    # FileUtils.copy_entry(src, dest)
+    # system("find #{dest} -type f -name '*.jpg' -exec sips -Z 48 {} \\;", out: :close, err: :close)
   end
 end
 
@@ -285,7 +291,7 @@ def build_media
   task 'Build Media' do
     dest_dir = "#{$tmp_dir}/thumbs"
     create_or_clean(dest_dir)
-    build_thumbnail_images("#{$src_dir}/media", "#{dest_dir}")
+    build_thumbnail_images("#{$src_dir}/media", dest_dir)
   end
 end
 
@@ -296,7 +302,7 @@ def build_app
     base_path = base_path[0..-2] if base_path.end_with?('/')
 
     create_or_clean(build_dir)
-    copy($src_dir, build_dir, exclude: %w(download fonts ical media webapp))
+    copy($src_dir, build_dir, exclude: %w[download fonts ical media webapp])
 
     FileUtils.cd(build_dir) do
       options = {
@@ -304,19 +310,18 @@ def build_app
         base_path: base_path,
         canonical_path: config['canonical_path'],
         icons: (Dir.glob('assets/icons/*.svg').collect { |f| [File.basename(f), load_svg(f)] }).to_h,
-        index_page: File.read('index.html').scan(/data\-app\-index\-page\s*\=\s*\"((?:\w|\-)*)\"/).flatten.first,
+        index_page: File.read('index.html').scan(/data-app-index-page\s*=s*"((?:\w|-)*)"/).flatten.first,
         placeholder_image_template: load_svg('assets/templates/placeholder.svg')
       }
-      build_html('index.html', options.merge( { meta_robots: config['meta_robots'] } ))
+      build_html('index.html', options.merge({ meta_robots: config['meta_robots'] }))
       Dir.glob('*.html').grep(/\d{3}\.html/) { |filename| build_html(filename, options) }
       Dir.glob('articles/*.html') { |filename| build_html(filename, options) }
-      # build_service_worker_js('articles', options)
       build_robots_txt(options)
       build_sitemap_xml('articles', options)
       build_htaccess('articles', options)
     end
 
-    copy(build_dir, $dest_dir, exclude: %w(assets))
+    copy(build_dir, $dest_dir, exclude: %w[assets])
   end
 end
 
@@ -324,7 +329,7 @@ def deploy
   task 'Deploy' do |config|
     Net::FTP.open(config['host'], config['username'], config['password']) do |ftp|
       ftp.chdir(config['remote_dir'])
-      ftp_upload(ftp, "#{$dest_dir}", '.')
+      ftp_upload(ftp, $dest_dir, '.')
       ftp_upload(ftp, "#{$src_dir}/download", 'download', exclude: /.*\.docx/)
       ftp_upload(ftp, "#{$src_dir}/ical", 'ical')
       ftp_upload(ftp, "#{$src_dir}/media", 'media')
@@ -339,8 +344,6 @@ begin
   build_media if $options[:build_media] == true
   build_app
   deploy if $options[:deploy] == true
-#rescue StandardError => e
-#  puts "\nError: #{e.message}"
 end
 
 puts
